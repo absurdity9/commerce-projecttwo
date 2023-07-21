11847e496665efa5e7d427bcb66db6dd317e95bf
absurdity9 <kfd@outlook.com>
2023-07-21 22:32:13 +0100

    added items model

--- auctions/models.py
+++ auctions/models.py
@@ -5,10 +5,6 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Items(models.Model):
-    title = models.CharField(max_length=200)
-    desc = models.CharField(max_length=500)
-    floor_price = models.DecimalField(max_digits=10, decimal_places=2)
-    photo_url = models.URLField
+
     
     
