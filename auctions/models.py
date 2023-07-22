1108e621f2f8a8c6c41f398f26175376722468f7
absurdity9 <kfd@outlook.com>
2023-07-22 08:46:00 +0100

    added listings and items model

--- auctions/models.py
+++ auctions/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-
+class Items(models.Model):
+    title = models.CharField(max_length=200)
+    desc = models.CharField(max_length=500)
+    floor_price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo_url = models.URLField
     
-    
+class Listing(models.Model):
+    item = models.ForeignKey(Items, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_end = models.DateTimeField
