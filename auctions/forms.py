3195dca7ef88f7614397fe09045aa5e45b09d7a6
absurdity9 <kfd@outlook.com>
2023-07-22 22:57:35 +0100

    added form logic, fx to create items, listing

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 
 # Form for creating a listing
-
 class createForm (forms.Form):
     title = forms.CharField(label="Your item name", max_length=200)
     desc = forms.CharField(label="Description of the item", max_length=500)
     floor_price = forms.CharField(label="Minimum Price")
-    photo_url = forms.URLField
-    date_end = forms.DateField
+    photo_url = forms.URLField()
+    date_end = forms.DateField()
--- auctions/views.py
+++ auctions/views.py
@@ -2,9 +2,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.urls import reverse
 from django import forms
 from .models import User
+from .models import Items
+from .models import Listing
 from .forms import createForm
 
 def index(request):
@@ -61,12 +64,29 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+def createItems_and_Listing(data):
+    title = data["title"]
+    desc = data["desc"]
+    floor_price = data["floor_price"]
+    photo_url = data["photo_url"]
+    date_end = data["date_end"]
+    
+    # create an item record
+    item = Item.objects.create(title=title, desc=desc, floor_price=floor_price, photo_url=photo_url)
+    
+    listing = Listing.objects.create(item=item, date_end=date_end)
+    
+    return item, listing
+
+
 def createListing(request):
     if request.method == "Post":
         form = createForm(request.POST)
         if form.is_valid():
-            pass
+            form_data = form.cleaned_data
+            item, listing = createItems_and_Listing (form_data)
+            return redirect("Success")
         else:
-            pass
+            create_listing_form = createForm()
     else:
         return render(request, "auctions/createlisting.html", {"create_listing_form": createForm})
